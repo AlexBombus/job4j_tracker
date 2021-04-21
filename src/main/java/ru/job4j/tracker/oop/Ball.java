397455c49b7b6fc57ebdcb0358c559821c30778a
AlexBombus <78620532+AlexBombus@users.noreply.github.com>
2021-04-21 12:15:40 +0300

    1.6. Взаимодействие объектов [#516320]

---
+++ src/main/java/ru/job4j/tracker/oop/Ball.java
@@ -0,0 +1,7 @@
+package ru.job4j.tracker.oop;
+
+public class Ball {
+    public void runBall(Ball ball) {
+        System.out.println("Колобок от дедушки и бабушки ушел,");
+    }
+}
---
+++ src/main/java/ru/job4j/tracker/oop/Fox.java
@@ -0,0 +1,7 @@
+package ru.job4j.tracker.oop;
+
+public class Fox {
+    public void tryEat(Ball ball) {
+        System.out.println("А от лисы не ушел, она его сьела.");
+    }
+}

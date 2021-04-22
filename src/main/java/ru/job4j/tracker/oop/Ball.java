ca8d83af05e32dd6d37774600303a36824478fcf
AlexBombus <78620532+AlexBombus@users.noreply.github.com>
2021-04-22 14:41:40 +0300

    terminal testing

--- src/main/java/ru/job4j/tracker/oop/Ball.java
+++ src/main/java/ru/job4j/tracker/oop/Ball.java
@@ -2,6 +2,6 @@ package ru.job4j.tracker.oop;
 
 public class Ball {
     public void runBall(Ball ball) {
-        System.out.println("Колобок и от дедушки и бабушки ушел,");
+        System.out.println("Колобок и от дедушки ущел, и от бабушки ушел,");
     }
 }

02d178edf4369f4e79beac1599f4f213026f0655
AlexBombus <78620532+AlexBombus@users.noreply.github.com>
2021-04-23 21:13:17 +0300

    CheckStyle

--- src/main/java/ru/job4j/tracker/inheritance/Doctor.java
+++ src/main/java/ru/job4j/tracker/inheritance/Doctor.java
@@ -2,10 +2,12 @@ package ru.job4j.tracker.inheritance;
 
 public class Doctor extends Profession {
     private int experience;
+
     public Doctor(String name, String surname, String education, String birthday, int experience) {
         super(name, surname, education, birthday);
         this.experience = experience;
     }
+
     public void doHeal() {
    }
 }

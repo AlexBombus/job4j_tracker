ea192181fc2de015c0684df44887aa2d0300e824
AlexBombus <78620532+AlexBombus@users.noreply.github.com>
2021-04-28 22:20:43 +0300

    2.4. Реализация класса StartUI. Вывод всех заявок [#516271]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -17,7 +17,17 @@ public class StartUI {
                 String name = scanner.nextLine();
                 Item item = new Item(name);
                 tracker.add(item);
-                System.out.println("Item is added: " + item);
+                System.out.println("Item is added: " + item + ".");
+            } else if (select == 1) {
+                System.out.println("=== Show all items ====");
+                Item[] items = tracker.findAll();
+                if (items.length > 0) {
+                    for (Item item : items) {
+                        System.out.println(item + ";");
+                    }
+                } else {
+                    System.out.println("Items list is empty.");
+                }
             } else if (select == 6) {
                 run = false;
             }

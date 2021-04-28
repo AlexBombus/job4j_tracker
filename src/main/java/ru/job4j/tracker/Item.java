f5711ca4f8f404162072b652e11d421351ea3a13
AlexBombus <78620532+AlexBombus@users.noreply.github.com>
2021-04-28 22:08:46 +0300

    2.3. Реализация класса StartUI. Добавление заявки [#516270]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -39,7 +39,7 @@ public class Item {
     @Override
     public String toString() {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
-        return "Item  {" + "id = " + id + ", name = '" + name + '\''
+        return "Item {" + "id = " + id + ", name = '" + name + '\''
                 + ", created = " + created.format(formatter) + '}';
     }
 

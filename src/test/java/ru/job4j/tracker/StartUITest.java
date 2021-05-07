8aa376886ea4faf5c315508e9214744184e7c67d
AlexBombus <78620532+AlexBombus@users.noreply.github.com>
2021-05-08 00:56:15 +0300

    1.1. Тесты на StartUI. [#516290]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -7,6 +7,30 @@ import static org.hamcrest.core.Is.is;
 public class StartUITest {
 
     @Test
+    public void whenInvalidExit() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"7", "0"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = new UserAction[]{
+                new ExitAction(out)
+        };
+        new StartUI(out).init(in, tracker, actions);
+        String ln = System.lineSeparator();
+        MatcherAssert.assertThat(out.toString(), is(
+                "Menu:" + ln
+                        + "0. Exit Program" + ln
+                        + "Wrong input, you can select: 0 .. 0" + ln
+                        + "Menu:" + ln
+                        + "0. Exit Program" + ln
+                        + "=== Exit Program ===" + ln
+                )
+        );
+    }
+
+
+    @Test
     public void whenFindAll() {
         Output out = new StubOutput();
         Input in = new StubInput(

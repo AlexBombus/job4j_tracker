2e93d1ee5397c502545b78437325203e0e90903c
AlexBombus <78620532+AlexBombus@users.noreply.github.com>
2021-05-21 01:24:21 +0300

    0. Сортировка [#10096]

---
+++ src/test/java/ru/job4j/tracker/SortDownByNameItemsTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class SortDownByNameItemsTest {
+
+    @Test
+    public void compare() {
+        Item item1 = new Item("Petya");
+        Item item2 = new Item("Olya");
+        Item item3 = new Item("Vasya");
+        List<Item> input = Arrays.asList(item1, item2, item3);
+        Collections.sort(input, new SortDownByNameItems());
+        assertEquals(input.get(0).getName(), "Vasya");
+        assertEquals(input.get(1).getName(), "Petya");
+        assertEquals(input.get(2).getName(), "Olya");
+    }
+}
---
+++ src/test/java/ru/job4j/tracker/SortUpByNameItemsTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.tracker;
+
+import org.hamcrest.MatcherAssert;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.*;
+
+import static org.junit.Assert.*;
+
+public class SortUpByNameItemsTest {
+
+    @Test
+    public void compare() {
+        Item item1 = new Item("Vasya");
+        Item item2 = new Item("Petya");
+        Item item3 = new Item("Olya");
+        List<Item> input = Arrays.asList(item1, item2, item3);
+        Collections.sort(input, new SortUpByNameItems());
+        assertEquals(input.get(0).getName(), "Olya");
+        assertEquals(input.get(1).getName(), "Petya");
+        assertEquals(input.get(2).getName(), "Vasya");
+    }
+}

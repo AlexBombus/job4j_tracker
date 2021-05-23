ed69416964ee8cc7b04832a75d2e8e3346554510
AlexBombus <78620532+AlexBombus@users.noreply.github.com>
2021-05-23 21:32:59 +0300

    4. Сортировка номера [#221284 #159725]

--- src/test/java/ru/job4j/collection/LexSort.java
+++ src/test/java/ru/job4j/collection/LexSort.java
@@ -1,29 +1,16 @@
 package ru.job4j.collection;
 
+import java.util.Arrays;
 import java.util.Comparator;
 
 public class LexSort implements Comparator<String> {
 
     @Override
     public int compare(String left, String right) {
-        StringBuilder leftNum = new StringBuilder();
-        for (int i = 0; i < left.length(); i++) {
-            if (Character.isDigit(left.charAt(i))) {
-                leftNum.append(left.charAt(i));
-            } else {
-                break;
-            }
-        }
-        StringBuilder rightNum = new StringBuilder();
-        for (int i = 0; i < right.length(); i++) {
-            if (Character.isDigit(right.charAt(i))) {
-                rightNum.append(right.charAt(i));
-            } else {
-                break;
-            }
-        }
-        int leftLexNum = Integer.parseInt(leftNum.toString());
-        int rightLexNum = Integer.parseInt(rightNum.toString());
+        String[] leftWord = left.split("\\.");
+        String[] rightWord = right.split("\\.");
+        int leftLexNum = Integer.parseInt(leftWord[0]);
+        int rightLexNum = Integer.parseInt(rightWord[0]);
         return Integer.compare(leftLexNum, rightLexNum);
     }
 }

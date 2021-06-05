779e6abccfdbc44357517dd1aff6e9b3a9990a88
AlexBombus <78620532+AlexBombus@users.noreply.github.com>
2021-06-05 13:50:45 +0300

    6. Функции высшего порядка [#24255]

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -1,7 +1,13 @@
 package ru.job4j.search;
 
 import java.util.ArrayList;
+import java.util.function.Predicate;
 
+/**
+ * Класс формирует список абонентов.
+ * Содержит метод по добавлению абонента в список абонентов
+ * и метод поиска абонента  в списке по ключу.
+ */
 public class PhoneDictionary {
     private ArrayList<Person> persons = new ArrayList<>();
 
@@ -9,7 +15,34 @@ public class PhoneDictionary {
         this.persons.add(person);
     }
 
+    /**
+     *Метод фильтрует список абонентов по содержанию key в переменной объекта.
+     * Метод поиска фильтрует все поля объекта Person.
+     * Реализацию метода переписали с использованием функции высшего порядка.
+     * с применением функционального интерфейса java.util.function.Predicate и лямбда.
+     *
+     * @param key значение по содержанию которого осуществляем фильтрацию
+     * @return список абонентов содержащих key в переменной.
+     */
     public ArrayList<Person> find(String key) {
+        ArrayList<Person> result = new ArrayList<>();
+        Predicate<Person> combine =(person) -> (
+                person.getName().contains(key)
+                        || person.getSurname().contains(key)
+                        || person.getPhone().contains(key)
+                        || person.getAddress().contains(key)
+        );
+        for (Person person : persons) {
+            if (combine.test(person)) {
+                result.add(person);
+            }
+        }
+        return result;
+    }
+}
+ /*
+ Предыдущая реализация метода:
+ public ArrayList<Person> find(String key) {
         ArrayList<Person> result = new ArrayList<>();
         for (Person person : persons) {
             if (person.getName().contains(key)
@@ -21,4 +54,4 @@ public class PhoneDictionary {
         }
         return result;
     }
-}
+  */

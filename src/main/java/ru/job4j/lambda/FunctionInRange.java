d79ecaf34ca54dd5584d082805019f575549c5e8
AlexBombus <78620532+AlexBombus@users.noreply.github.com>
2021-06-05 12:20:31 +0300

    5. Подсчет функции в диапазоне. [#24251 #164694]

--- src/main/java/ru/job4j/lambda/FunctionInRange.java
+++ src/main/java/ru/job4j/lambda/FunctionInRange.java
@@ -17,13 +17,13 @@ public class FunctionInRange {
      * @param function  заданная функция расчета (молжет быть линейной, квадратичной и тд).
      * @return список значений результатов расчета функции.
      */
-    public static List<Double> diapason(int start, int end, Function<Double, Double> function){
-       /*
-       При использовании идентификатора var - компилятор проверяет правую часть от оператора присваивания,
-       определяет конкретный тип переменной и заменяет им var, что позволяет сократить шаблон кода.
-        */
+    public static List<Double> diapason(int start, int end, Function<Double, Double> function) {
+/*
+При использовании идентификатора var - компилятор проверяет правую часть от оператора присваивания,
+определяет конкретный тип переменной и заменяет им var, что позволяет сократить шаблон кода.
+*/
         var rsl = new ArrayList<Double>();
-        for(int i = start; i <= end; i++) {
+        for (int i = start; i <= end; i++) {
             rsl.add(function.apply(i * 1.0));
         }
         return rsl;

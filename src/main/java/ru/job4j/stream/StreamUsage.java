a5159afde2e3c15205ceb3c7de7521e2fa002151
AlexBombus <78620532+AlexBombus@users.noreply.github.com>
2021-06-05 19:09:01 +0300

    0. Stream API [#58166]

--- src/main/java/ru/job4j/stream/StreamUsage.java
+++ src/main/java/ru/job4j/stream/StreamUsage.java
@@ -7,11 +7,11 @@ import java.util.stream.Collectors;
  * С помощью stream()  создаем поток данных списка, метод filter() сортирует потока так,
  * чтобы в нем остались только положительные числа.Условия сортировки задается лямбда.
  * Метод collect(Collectors.toList()) позволяет полученный результат сохранить в коллекции типа List
+ * метод forEach(System.out::println) выводит отсортированный список в консоль.
  */
 public class StreamUsage {
     public static void main(String[] args) {
         List<Integer> numbers = List.of(3, 2, -5, 10, -78, 0);
-        List<Integer> sorted = numbers.stream().filter(num -> num > 0).collect(Collectors.toList());
-        System.out.println(sorted);
+        numbers.stream().filter(num -> num > 0).collect(Collectors.toList()).forEach(System.out::println);
     }
 }

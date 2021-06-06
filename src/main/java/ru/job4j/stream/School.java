e34f7dbf73c08a1fe5176d510622cad910f2ab2d
AlexBombus <78620532+AlexBombus@users.noreply.github.com>
2021-06-06 09:22:24 +0300

    1. Фильтрация учеников [#110058]

--- src/main/java/ru/job4j/stream/School.java
+++ src/main/java/ru/job4j/stream/School.java
@@ -6,11 +6,6 @@ import java.util.stream.Collectors;
 
 public class School {
 
-    private Predicate<Student> sort10A = student -> student.getScore() >= 70;
-    private Predicate<Student> sort10B
-            = student -> student.getScore() >= 50 && student.getScore() < 70;
-    private Predicate<Student> sort10V = student -> student.getScore() < 50;
-
     public List<Student> collect(List<Student> students, Predicate<Student> predict) {
         return students.stream()
                 .filter(predict)

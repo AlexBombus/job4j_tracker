c6b32cf588b83b21c8185d1162f56d6c37238af7
AlexBombus <78620532+AlexBombus@users.noreply.github.com>
2021-06-06 22:07:51 +0300

    3. Уникальность элементов и сортировка. [#110225 #165196]

--- src/main/java/ru/job4j/stream/travelcompany/Profiles.java
+++ src/main/java/ru/job4j/stream/travelcompany/Profiles.java
@@ -5,16 +5,26 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Profiles {
-
+    /**
+     * Метод формирует список адресов из списка профайлов клиентов,
+     * @param profiles список профайлов клиентов
+     * @return  список адресов клиентов
+     */
     public List<Address> collect(List<Profile> profiles) {
         return profiles.stream().map(Profile::getAddress).collect(Collectors.toList());
     }
 
+    /**
+     * Метод формирует список адресов из списка профайлов клиентов,
+     * при этом  оставляет только уникальные(неповоряющиеся) элементы.
+     * @param profiles список профайлов клиентов
+     * @return список адресов без повторяющихся значений
+     */
     public List<Address> collectUniqSortedByCity(List<Profile> profiles) {
-        return profiles.stream()
-                .map(Profile::getAddress)
-                .sorted(Comparator.comparing(Address::getCity))
-                .distinct()
-                .collect(Collectors.toList());
+        return profiles.stream()    // создае поток данных из List<Profile> profiles
+                .map(Profile::getAddress) // принимает элемент потока Profile, а возвращает другой эл]. - адрес
+                .sorted(Comparator.comparing(Address::getCity)) // сорируе элементы по полю String city.
+                .distinct() // осавляе олько уникальные элементы исключая дубликаы
+                .collect(Collectors.toList()); // ообранные элеменв собираюся в список List
     }
 }

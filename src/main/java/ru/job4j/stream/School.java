3c2ab723824cbc3eb2a19d30269040831c5bd78c
AlexBombus <78620532+AlexBombus@users.noreply.github.com>
2021-06-06 11:06:00 +0300

    1. Фильтрация учеников [#110058]

--- src/main/java/ru/job4j/stream/School.java
+++ src/main/java/ru/job4j/stream/School.java
@@ -4,8 +4,33 @@ import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
+/**
+ * Класс создан для решения задачи:
+ * В школе пришло требование разделить 9А класс на три класса.
+ * У каждого ученика есть общий балл по всем предметам.  int score;
+ * score - может быть в диапазоне от 0 до 100.
+ * Учеников нужно поделить на следующие группы.
+ * 10А диапазон балла [70: 100],
+ * 10Б диапазон балла [50: 70);
+ * 10B диапазон балла (0: 50);
+ */
 public class School {
 
+    /*
+    для решения задачи можно использовать предикаты:
+    private Predicate<Student> sort10A = student -> student.getScore() >= 70;
+    private Predicate<Student> sort10B
+            = student -> student.getScore() >= 50 && student.getScore() < 70;
+    private Predicate<Student> sort10V = student -> student.getScore() < 50;
+
+     */
+
+    /**
+     * Метод осуществляет фильтрацию учеников по заданному предикату
+     * @param students входящий список всех студентов
+     * @param predict предикат с заданным условием отбора
+     * @return возвращаемый список отобранных студентов
+     */
     public List<Student> collect(List<Student> students, Predicate<Student> predict) {
         return students.stream()
                 .filter(predict)

b5bc8da0a9d49e82c655aa43f132d3a818aa76ba
AlexBombus <78620532+AlexBombus@users.noreply.github.com>
2021-06-06 20:22:49 +0300

    5. Преобразование матрицы чисел в список чисел. [#110228]

--- src/test/java/ru/job4j/stream/MatrixTest.java
+++ src/test/java/ru/job4j/stream/MatrixTest.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 import java.util.List;
 
 public class MatrixTest {
-
     @Test
     public void whenConvertSuccessful() {
         Integer[][] numbers = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};

9da59b491e6241720f42d4635f6a3def0abd9a7c
AlexBombus <78620532+AlexBombus@users.noreply.github.com>
2021-06-07 13:14:32 +0300

    3. Уникальность элементов и сортировка. [#110225 #165196]

---
+++ src/main/java/ru/job4j/stream/StreamFilterCollect.java
@@ -0,0 +1,20 @@
+package ru.job4j.stream;
+
+import java.util.List;
+import java.util.stream.Collectors;
+/**
+ * Используем Stream API  совместно с лямбда-выражением для фильтрации списка чисел.
+ * С помощью stream()  создаем поток данных списка, метод filter() сортирует потока так,
+ * чтобы в нем остались только положительные числа.Условия сортировки задается лямбда.
+ * Метод collect(Collectors.toList()) позволяет полученный результат сохранить в коллекции типа List
+ * метод forEach(System.out::println) выводит отсортированный список в консоль.
+ */
+public class StreamFilterCollect {
+    public static void main(String[] args) {
+        List<Integer> numbers = List.of(3, 2, -5, 10, -78, 0);
+        numbers.stream()
+                .filter(num -> num > 0)
+                .collect(Collectors.toList())
+                .forEach(System.out::println);
+    }
+}
--- src/main/java/ru/job4j/stream/StreamUsage.java
+++ src/main/java/ru/job4j/stream/StreamUsage.java
@@ -1,20 +1,28 @@
 package ru.job4j.stream;
 
 import java.util.List;
-import java.util.stream.Collectors;
-/**
- * Используем Stream API  совместно с лямбда-выражением для фильтрации списка чисел.
- * С помощью stream()  создаем поток данных списка, метод filter() сортирует потока так,
- * чтобы в нем остались только положительные числа.Условия сортировки задается лямбда.
- * Метод collect(Collectors.toList()) позволяет полученный результат сохранить в коллекции типа List
- * метод forEach(System.out::println) выводит отсортированный список в консоль.
- */
+
 public class StreamUsage {
+    public static class Task {
+        private final String name;
+        private final long spent;
+
+        public Task(String name, long spent) {
+            this.name = name;
+            this.spent = spent;
+        }
+    }
+
     public static void main(String[] args) {
-        List<Integer> numbers = List.of(3, 2, -5, 10, -78, 0);
-        numbers.stream()
-                .filter(num -> num > 0)
-                .collect(Collectors.toList())
+        List<Task> tasks = List.of(
+                new Task("Bug #1", 10),
+                new Task("Task #2", 20),
+                new Task("Bug #3", 40)
+        );
+        tasks.stream()
+                .filter(task -> task.name.contains("Bug"))
+                .filter(task -> task.spent > 30)
+                .map(task -> task.name + " " + task.spent)
                 .forEach(System.out::println);
     }
 }

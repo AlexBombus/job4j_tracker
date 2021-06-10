e5edcddea43e8e11d5e82a9844da7530e5d9fadf
AlexBombus <78620532+AlexBombus@users.noreply.github.com>
2021-06-11 01:35:54 +0300

    2. Аттестация [#49777]

---
+++ src/main/java/ru/job4j/attestation/Analyze.java
@@ -0,0 +1,121 @@
+package ru.job4j.attestation;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * Класс Analyze получает статистику по аттестатам.
+ */
+public class Analyze {
+    /**
+     * Метод averageScore  вычисляет общий средний балл.
+     * для реализации используются методы:
+     * - flatMap() для преобразования в поток объектов Subject;
+     * - mapToInt() для последующего преобразования в потом оценок по каждому предмету;
+     * - average() для расчета среднего бала по предмету;
+     * - orElse() для того чтобы вернуть значение по умолчанию.
+     * @param stream список учеников;
+     * @return средний балл
+     */
+    public static double averageScore(Stream<Pupil> stream) {
+        return stream
+                .flatMap(pupil -> pupil.getSubjects().stream())
+                .mapToInt(Subject::getScore)
+                .average()
+                .orElse(0D);
+    }
+
+    /**
+     * Метод averageScoreBySubject вычисляет средний балл ученика по его предметам.
+     *поскольку надо выполнить преобразование в поток объектов класса Tuple,
+     * последовательность реализации следующая:
+     * - метод map() преобразует в поток объекты класса Tuple,
+     * внутри метода мы создаем эти объекты - там  фигурирует строка new Tuple();
+     *
+     * - в конструктор первым параметром передается имя текущего объекта Pupil - используем геттер;
+     * - вторым параметром рассчитанный средний балл - расчет как для метода averageScore;
+     * - последний метод collect(), с помощью которого все собираем в коллекцию List.
+     * @param stream список учеников;
+     * @return  Возвращает список объектов Tuple (имя ученика и средний балл).
+     */
+    public static List<Tuple> averageScoreBySubject(Stream<Pupil> stream) {
+        return stream.map(pupil -> new Tuple(
+                pupil.getName(), pupil.getSubjects()
+                .stream()
+                .mapToInt(Subject::getScore)
+                .average()
+                .orElse(0D)))
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * Метод averageScoreByPupil вычисляет средний балл по всем предметам для каждого ученика.
+     *- flatMap() для преобразования в поток объектов Subject;
+     * - метод collect() в который передаем метод groupingBy() класса Collectors.
+     * При этом карта собирается следующим образом: ключ - это имя предмета,
+     * значение - средний балл по этому предмету для каждого ученика.
+     * Для расчета среднего балла используем метод averagingDouble() класса Collectors;
+     * - собранную карту  разбираем с помощью entrySet() и открываем поток с помощью stream();
+     * - полученный поток с помощью map() преобразуем в поток объектов класса Tuple,
+     * внутри метода создаем эти объекты - там фигурирует строка new Tuple();
+     * - в конструктор передаем параметры с помощью методов getKey() и getValue() интерфейса Entry;
+     * - последний метод collect(), с помощью которого все собираем в коллекцию List.
+     *
+     * @param stream список учеников;
+     * @return  Возвращает список объектов Tuple (название предмета и средний балл).
+     */
+    public static List<Tuple> averageScoreByPupil(Stream<Pupil> stream) {
+        return stream.flatMap(pupil -> pupil.getSubjects().stream())
+                .collect(Collectors.groupingBy(
+                        Subject::getName, Collectors.averagingDouble(Subject::getScore)))
+                .entrySet()
+                .stream()
+                .map(entry -> new Tuple(entry.getKey(), entry.getValue()))
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * Метод bestStudent - возвращает лучшего ученика.
+     * Лучшим считается ученик с наибольшим баллом по всем предметам.
+     * Рассчитываем суммарный балл, а не средний -поэтому вместо average() используем метод sum();
+     * Терминальной операцией является не метод collect(), а использование метода max(),
+     * в который передается компаратор. При этом компаратор определяет объект Tuple,
+     * у которого значение Score  максимальное;
+     * - orElse() для возврата значения по умолчанию.
+     *
+     * @param stream список учеников;
+     * @return возсращает объект Tuple;
+     */
+    public static Tuple bestStudent(Stream<Pupil> stream) {
+        return stream.map(t -> new Tuple(
+                t.getName(), t.getSubjects()
+                .stream()
+                .mapToInt(Subject::getScore)
+                .sum()))
+                .max(Comparator.comparing(Tuple::getScore))
+                .orElse(null);
+    }
+
+ /**
+ * Метод bestSubject - возвращает предмет с наибольшим баллом для всех студентов.
+ *- рассчитываем суммарный балл, не средний - вместо averagingDouble() используем summingDouble();
+  * - терминальная операция  не метод collect(), а  метод max(), в который передается компаратор.
+  * Компаратор определяет объект Tuple, у которого значение Score  максимальное;
+  * - orElse() для того чтобы вернуть значение по умолчанию.
+  *
+ * @param stream список учеников;
+ * @return Возвращает объект Tuple (имя предмета, сумма баллов каждого ученика по этому предмету)
+ */
+    public static Tuple bestSubject(Stream<Pupil> stream) {
+        return stream.flatMap(s -> s.getSubjects().stream())
+                .collect(Collectors.groupingBy(
+                        Subject::getName, Collectors.summingDouble(Subject::getScore)))
+                .entrySet()
+                .stream()
+                .map(t -> new Tuple(t.getKey(), t.getValue()))
+                .max(Comparator.comparing(Tuple::getScore))
+                .orElse(null);
+    }
+}
---
+++ src/test/java/ru/job4j/attestation/AnalyzeTest.java
@@ -0,0 +1,91 @@
+package ru.job4j.attestation;
+
+import static org.hamcrest.Matchers.is;
+
+import org.hamcrest.MatcherAssert;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+public class AnalyzeTest {
+
+    @Test
+    public void whenSinglePupil() {
+        double average = Analyze.averageScore(
+                List.of(
+                        new Pupil("Ivanov", List.of(new Subject("Math", 100)))
+                ).stream()
+        );
+        MatcherAssert.assertThat(average, is(100D));
+    }
+
+    @Test
+    public void whenPupilAverage() {
+        double average = Analyze.averageScore(
+                List.of(
+                        new Pupil("Ivanov", List.of(new Subject("Math", 100))),
+                        new Pupil("Petrov", List.of(new Subject("Math", 60)))
+                ).stream()
+        );
+        MatcherAssert.assertThat(average, is(80D));
+    }
+
+    @Test
+    public void whenListOfPupilAverage() {
+        List<Tuple> average = Analyze.averageScoreBySubject(
+                List.of(
+                        new Pupil("Ivanov", List.of(new Subject("Math", 100),
+                                new Subject("Lang", 100))),
+                        new Pupil("Petrov", List.of(new Subject("Math", 60),
+                                new Subject("Lang", 60)))
+                ).stream()
+        );
+        MatcherAssert.assertThat(average, is(List.of(
+                new Tuple("Ivanov", 100D),
+                new Tuple("Petrov", 60D)
+        )));
+    }
+
+    @Test
+    public void whenListOfSubjectAverage() {
+        List<Tuple> average = Analyze.averageScoreByPupil(
+                List.of(
+                        new Pupil("Ivanov", List.of(new Subject("Math", 100),
+                                new Subject("Lang", 100))),
+                        new Pupil("Petrov", List.of(new Subject("Math", 60),
+                                new Subject("Lang", 60)))
+                ).stream()
+        );
+        Assert.assertEquals(average, List.of(
+                new Tuple("Lang", 80D),
+                new Tuple("Math", 80D)
+        ));
+    }
+
+    @Test
+    public void whenBestPupil() {
+        Tuple best = Analyze.bestStudent(
+                List.of(
+                        new Pupil("Ivanov", List.of(new Subject("Math", 100),
+                                new Subject("Lang", 100))),
+                        new Pupil("Petrov", List.of(new Subject("Math", 60),
+                                new Subject("Lang", 60)))
+                ).stream()
+        );
+        MatcherAssert.assertThat(best, is(new Tuple("Ivanov", 200D)));
+    }
+
+    @Test
+    public void whenBestSubject() {
+        Tuple best = Analyze.bestSubject(
+                List.of(
+                        new Pupil("Ivanov", List.of(new Subject("Math", 100),
+                                new Subject("Lang", 40))),
+                        new Pupil("Petrov", List.of(new Subject("Math", 60),
+                                new Subject("Lang", 60)))
+                ).stream()
+        );
+        MatcherAssert.assertThat(best, is(new Tuple("Math", 160D)));
+    }
+}
